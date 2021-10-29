0aa3502ab084a86fdf1a62fa7f402d2074566d02
Pankaj K Borade <62588059+BkPankaj@users.noreply.github.com>
2021-10-30 00:37:43 +0530

    Question_2_New_Pankaj_Borade
    Applied your condition as you told

--- Python basics/Lists/QUESTION_2_Solution.py
+++ Python basics/Lists/QUESTION_2_Solution.py
@@ -1,9 +1,23 @@
 from math import sqrt
 
-up = int(input("UP "))
-down = int(input("DOWN "))
-left = int(input("LEFT "))
-right = int(input("RIGHT "))
+x = 0
+y = 0
+up = 0
+down = 0
+left = 0
+right = 0
+
+for x in range(0,4):
+    m, n = map(str,input().split())
+    query = m.lower()
+    if "up" in query:
+     up = up +  int(n)
+    if "down" in query:
+     down = down + int(n)
+    if "left" in query:
+     left = left + int(n)
+    if "right" in query:
+     right = right + int(n)
 
 x_dist = 0 + right - left
 y_dist = 0 + up - down
