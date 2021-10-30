b458ca98c543c274f61bee920de50b71714b56b5
Swara Jain <89874563+Swarajain5@users.noreply.github.com>
2021-10-30 11:33:07 +0530

    QUES2 SOLN SWARA JAIN

--- Ques2_Swara Jain.py
+++ Ques2_Swara Jain.py
@@ -1,15 +1,22 @@
-# -*- coding: utf-8 -*-
-
 #ques 2
+
 print("give the direction to your robot")
-up =input("enter upward value:")
-down=input("enter downward value:")
-left=input("enter left value:")
-right=input("enter right value:")
+
 from math import sqrt
+for i in range(1,5):
+  if i<=5:
+    direction = input("enter the direction=")
+    direction = direction.lower()
+    if direction == "up":
+        up=input("enter the value for up=")
+    if direction == "down":
+        down=input("enter the value for down=")
+    if direction == "left":
+        left=input("enter the value for left=")
+    if direction == "right":
+        right=input("entr the value for right=")
 x1=int(up)-int(down)
 y1=int(left)-int(right)
 dist=int(sqrt((x1*x1)+(y1*y1)))
 print(dist)
 print(x1 , y1)
-
