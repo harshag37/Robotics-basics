dad57e67b3fdbcca3dbc8070a40de1e885b1de20
elqmausam <90000132+elqmausam@users.noreply.github.com>
2021-10-31 09:41:38 +0530

    Update firstque.py

--- firstque.py
+++ firstque.py
@@ -1,7 +1,9 @@
+import string
+
 password = input()
 
-upper_case = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-lower_case = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+upper_case = list(string.ascii_uppercase)
+lower_case = list(string.ascii_lowercase)
 digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbols = ["@", "#", "$"]
 
