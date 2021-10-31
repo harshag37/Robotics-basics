e0b2239631a47ecb8865865eef448a4dcb1448b0
elqmausam <90000132+elqmausam@users.noreply.github.com>
2021-10-31 10:59:40 +0530

    Update secondque.py

--- secondque.py
+++ secondque.py
@@ -2,13 +2,13 @@ l=[0,0]
 for i in range(4):
     direction,value = input().split(" ")
     if direction=="UP":
-        l+=value[1]
+        l+=int(value[1])
     elif direction=="DOWN":
-        l-=value[1]
+        l-=int(value[1])
     elif direction=="RIGHT":
-        l+=value[0]
+        l+=int(value[0])
     elif direction=="LEFT":
-        l-=value[0]
+        l-=int(value[0])
 d=(l[0]**2+l[1]**2)**0.5
 print(d)
     
