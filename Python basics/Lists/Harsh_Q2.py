2f482bedd1c48dfb614d9b663aa8dd7654cc931f
harshag37 <harshag37@gmail.com>
2021-11-06 13:54:19 +0530

    edited

--- Python basics/Lists/Harsh_Q2.py
+++ Python basics/Lists/Harsh_Q2.py
@@ -1,13 +1,16 @@
-l=[0,0]
-for i in range(4):
-    direction,value = input().split(" ")
-    if direction=="UP":
-        l[1]+=int(value)
-    elif direction=="DOWN":
-        l[1]-=int(value)
-    elif direction=="RIGHT":
-        l[0]+=int(value)
-    elif direction=="LEFT":
-        l[0]-=int(value)
-d=(l[0]**2+l[1]**2)**0.5
-print(d)
+# #logic by harsh
+# l=[0,0]
+# for i in range(4):
+#     direction,value = input().split(" ")
+#     if direction=="UP":
+#         l[1]+=int(value)
+#     elif direction=="DOWN":
+#         l[1]-=int(value)
+#     elif direction=="RIGHT":
+#         l[0]+=int(value)
+#     elif direction=="LEFT":
+#         l[0]-=int(value)
+# d=(l[0]**2+l[1]**2)**0.5
+# print(d)
+n='h'
+print(ord(n))
