92e788488bd00dbdf70a2d1f92cdcdceffce3358
elqmausam <90000132+elqmausam@users.noreply.github.com>
2021-11-15 14:11:11 +0530

    Update Saniya_01.py

--- Python basics/Lists/Saniya_01.py
+++ Python basics/Lists/Saniya_01.py
@@ -1,16 +1,18 @@
 n1 = int(input())
 n2 = int(input())
-t = 0
-for p in range(2, n1):
+l = []
+c = 0
+for p in range(1, n1):
     if n1%p == 0:
         if n2%p == 0:
-            t+=1
-            print(p)
+            l.append(p)
+        c = max(l)
+        
+        
+   
+print(c)
             
-    
-if t == 0:
-    print("1")
-         
+     
          
     
         
