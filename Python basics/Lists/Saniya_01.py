bab493a6cde5db11445528093f408328ff6bbcb0
elqmausam <90000132+elqmausam@users.noreply.github.com>
2021-11-19 11:41:59 +0530

    Update Saniya_01.py

--- Python basics/Lists/Saniya_01.py
+++ Python basics/Lists/Saniya_01.py
@@ -1,18 +1,12 @@
 n1 = int(input())
-n2 = int(input())
-l = []
+n2 = int(input()) 
+l = min(n1,n2)
 c = 0
-for p in range(1, n1):
-    if n1%p == 0:
-        if n2%p == 0:
-            l.append(p)
-        c = max(l)
+for p in range(1, l+1):
+    if n1%p == 0 and n2%p == 0:
+        c = p
         
-        
-   
 print(c)
-            
-     
          
     
         
